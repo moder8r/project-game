1111a4086279363d8bfbe0ee1cf16f1c56c389d7
moder8r <themoder8r@gmail.com>
2024-09-16 20:02:32 +0500

    added a player choice prompt

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ function getComputerChoice(computer) {
 }
 
 console.log(getComputerChoice(computer))
+
+const player = parseInt(prompt('Enter 1 for Rock, 2 for Paper and 3 for Scissors'))
+
+function getHumanChoice(player) {
+    if (player === 1) {
+        return 'Rock'
+    } else if (player === 2) {
+        return 'Paper'
+    } else if (player === 3) {
+        return 'Scissors'
+    } else {
+        return 'Incorrect Entry'
+    }
+}
+
+console.log(getHumanChoice(player));
